83d476be0d6ff34469fbcba36da33a9f8f3559ef
Timo Schwarzer <timo@schwarzer.dev>
2022-12-07 15:41:34 +0100

    Update image name to use ghcr.io

--- development-utils/image-to-k3d-cluster.sh
+++ development-utils/image-to-k3d-cluster.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-DOCKER_BUILDKIT=true docker build --build-arg=CARGO_PROFILE=dev -t timoschwarzer/btrfs-provisioner .
-k3d image import -m direct timoschwarzer/btrfs-provisioner
+DOCKER_BUILDKIT=true docker build --build-arg=CARGO_PROFILE=dev -t ghcr.io/timoschwarzer/btrfs-provisioner .
+k3d image import -m direct ghcr.io/timoschwarzer/btrfs-provisioner
--- src/config.rs
+++ src/config.rs
@@ -6,6 +6,6 @@ pub const PROVISIONER_NAME: &str = "timo.schwarzer.dev/btrfs-provisioner";
 pub const FINALIZER_NAME: &str = "timo.schwarzer.dev/btrfs-provisioner";
 pub const NODE_ANNOTATION_NAME: &str = "btrfs-provisioner.timo.schwarzer.dev/node";
 pub const NODE_HOSTNAME_KEY: &str = "kubernetes.io/hostname";
-pub const IMAGE: &str = "timoschwarzer/btrfs-provisioner";
+pub const IMAGE: &str = "ghcr.io/timoschwarzer/btrfs-provisioner";
 pub const SERVICE_ACCOUNT_NAME: &str = "btrfs-provisioner-service-account";
 pub const HOST_FS_ENV_NAME: &str = "HOST_FS";
