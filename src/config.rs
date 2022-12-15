b23198f227140a68326a346c8eea862a03454743
Timo Schwarzer <timo@schwarzer.dev>
2022-12-15 16:10:07 +0100

    Improve configuration environment variables

--- src/config.rs
+++ src/config.rs
@@ -14,13 +14,14 @@ lazy_static! {
     pub static ref VOLUMES_DIR: String = std::env::var("VOLUMES_DIR").unwrap_or_else(|_| "/volumes".into());
     pub static ref IMAGE: String = std::env::var("IMAGE").unwrap_or_else(|_| "ghcr.io/timoschwarzer/btrfs-provisioner".into());
     pub static ref ARCHIVE_ON_DELETE: bool = matches!(std::env::var("ARCHIVE_ON_DELETE").unwrap_or_else(|_| "false".into()).as_str(), "true" | "1");
+    pub static ref DYNAMIC_STORAGE_CLASS_ENABLED: bool = matches!(std::env::var("DYNAMIC_STORAGE_CLASS").unwrap_or_else(|_| "false".into()).as_str(), "true" | "1");
     pub static ref DYNAMIC_STORAGE_CLASS_NAME: String = std::env::var("DYNAMIC_STORAGE_CLASS_NAME").unwrap_or_else(|_| "btrfs-provisioner".into());
-    pub static ref STORAGE_CLASS_NAME_PATTERN: String = {
-        let pattern = std::env::var("STORAGE_CLASS_NAME_PATTERN").unwrap_or_else(|_| "btrfs-provisioner-{}".into());
-        assert!(pattern.contains("{}"), "STORAGE_CLASS_NAME_PATTERN must contain a {{}} placeholder");
+    pub static ref STORAGE_CLASS_PER_NODE_ENABLED: bool = matches!(std::env::var("STORAGE_CLASS_PER_NODE").unwrap_or_else(|_| "true".into()).as_str(), "true" | "1");
+    pub static ref STORAGE_CLASS_PER_NODE_NAME_PATTERN: String = {
+        let pattern = std::env::var("STORAGE_CLASS_PER_NODE_NAME_PATTERN").unwrap_or_else(|_| "btrfs-provisioner-{}".into());
+        assert!(pattern.contains("{}"), "STORAGE_CLASS_PER_NODE_NAME_PATTERN must contain a {{}} placeholder");
         pattern
     };
-    pub static ref STORAGE_CLASS_PER_NODE: bool = matches!(std::env::var("STORAGE_CLASS_PER_NODE").unwrap_or_else(|_| "true".into()).as_str(), "true" | "1");
 }
 
 // Job labeling
